a0ca84fd3f3ad538db4d23e4e7b12ed30f87c47a
Joe Fabre <joefabre@Joes-MacBook-Pro.local>
2021-01-16 20:51:47 -0500

    step4

---
+++ Testing/Testing/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System; //using directive
+
+namespace Testing.Models //namespace
+{
+    public class Product //class
+    {
+        public Product() //method
+        {
+        }
+        //properties
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+    }
+}
