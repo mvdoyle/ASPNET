0017d9d88f4678e33f25b6c950eca35a46d14a89
Eber27 <eberfreire@gmail.com>
2021-07-09 14:09:15 -0500

    adventure_works_proj

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int Key);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
---
+++ Testing/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class Category
+    {
+        public int ProductSubcategoryKey { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
